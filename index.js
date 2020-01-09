dbf17a804a7a66c93c3e1df25c8782f687660067
juliancm <jc.juliancastro@gmail.com>
2020-01-08 21:37:26 -0600

    added some js functionality

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function test () {
     console.log('chicken');
     $('.contentDisplay').html(`
-        <section>
+    <section>
         <h2>Welcome to the Bicycle Quiz!</h2>
         <p>Think you know everything about bicycles?</p>
     </section>
@@ -12,12 +12,69 @@ function startQuizClick () {
     $('.startQuiz').on('click', function(event) {
         event.preventDefault();
         $('.contentDisplay').html(displayNextQuestion());
-        $('')
+        $('.alertArea').html(`
+        
+        `)
     });
 }
 
 function displayNextQuestion () {
-    $('.contentDisplay').html(`<p>Hello</p>`);
+    $('.contentDisplay').html(`
+        <form>
+        <fieldset class="answerDisplay">
+            <legend>Quiz Intro/Questions goes here/Right or Wrong Answer notification goes here</legend>
+
+            <label>Choose one option: </label>
+
+            <br>
+
+            <input type="radio"
+
+                   name="possibleAnswerTextOrIdForJs"
+
+                   id="referstoID"
+
+                   value="thisCorrespondsToIndividualElement"
+
+                   required
+
+            />
+            <!--- the name attribute needs to match all others in fieldset --->
+            <label for="referstoID">Answer text 1 goes here</label>
+
+            <br>
+
+            <input type="radio"
+
+                   name="possibleAnswerTextOrIdForJs"
+
+                   id="referstoID2"
+
+                   value="thisCorrespondsToIndividualElement"
+
+                   required
+            />
+            <!-- name gets sent back for the input block </> --->
+
+            <label for="referstoID2">Answer text 2 goes here</label>
+
+            <br>
+
+            <input type="radio"
+
+                   name="possibleAnswerTextOrIdForJs"
+
+                   id="referstoID3"
+
+                   value="thisCorrespondsToIndividualElement"/>
+
+            <label for="referstoID3">Answer text 3 goes here</label>
+
+
+
+        </fieldset>
+    </form>
+`);
 }
 
 function callBack () {
